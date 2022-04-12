03a22e5bd4882a0abe6d47d708015b5fc00c2d23
HP-Notebook-6061c336 <hpnotebook6061c336@localhost.localdomain>
2022-04-12 19:45:26 +0600

    test category test add view

--- resources/views/admin/fixed/sidebar.blade.php
+++ resources/views/admin/fixed/sidebar.blade.php
@@ -110,18 +110,36 @@
           
 
           <li class="side-nav-item">
-              <a data-bs-toggle="collapse" href="#sidebarTasks" aria-expanded="false" aria-controls="sidebarTasks" class="side-nav-link">
+              <a data-bs-toggle="collapse" href="#patients" aria-expanded="false" aria-controls="sidebarTasks" class="side-nav-link">
                   <i class="uil-clipboard-alt"></i>
                   <span> Patient </span>
                   <span class="menu-arrow"></span>
               </a>
-              <div class="collapse" id="sidebarTasks">
+              <div class="collapse" id="patients">
                   <ul class="side-nav-second-level">
                       <li>
                           <a href="{{route('patient.list')}}">Patients List</a>
                       </li>
                       <li>
                           <a href="apps-tasks-details.html">Admission</a>
+                      </li>
+                  </ul>
+              </div>
+          </li>
+
+          <li class="side-nav-item">
+              <a data-bs-toggle="collapse" href="#diagonistic" aria-expanded="false" aria-controls="sidebarTasks" class="side-nav-link">
+                  <i class="uil-clipboard-alt"></i>
+                  <span> Diagonistic </span>
+                  <span class="menu-arrow"></span>
+              </a>
+              <div class="collapse" id="diagonistic">
+                  <ul class="side-nav-second-level">
+                      <li>
+                          <a href="{{route('test.category.list')}}">Test Category</a>
+                      </li>
+                      <li>
+                          <a href="{{route('test.list')}}">Test</a>
                       </li>
                   </ul>
               </div>
---
+++ resources/views/admin/pages/diagonistic/test-list.blade.php
@@ -0,0 +1,45 @@
+@extends('admin.master')
+@section('content')
+<h1>Test List</h1>
+<hr>
+<a href="{{route('test.add')}}"><button class="btn btn-primary">Create Test</button></a>
+<br><br>
+<div> 
+  <table class="table" style="text-align: center;">
+    <thead class="thead-dark">
+      <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Test Category</th>
+      <th scope="col">Test</th>
+      <th scope="col">Price</th>
+      <th scope="col">Procedure</th>
+      <th scope="col">Description</th>
+      <th scope="col">Image</th>
+      <th scope="col">Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        @foreach($tests as $key=>$test)
+        
+        <th scope="row">{{$key+1}}</th>
+        <td>{{$test->category->name}}</td>
+        <td>{{$test->name}}</td>
+        <td>{{$test->price}}</td>
+        <td>{{$test->procedure}}</td>
+        <td>{{$test->description}}</td>
+        <td><img src="{{url('/test/'.$test->image)}}" style="border-radius:4px" width="100px" alt="test image"></td>
+        <td>
+          <a class="btn btn-success btn-sm" href=""><i class="fa-solid fa-eye"></i></a>
+          <a class="btn btn-warning btn-sm" href=""><i class="fas fa-edit"></i></a>
+          <a class="btn btn-danger btn-sm" href=""><i class="fas fa-trash"></i></a>
+      </td>
+        </td>
+      </tr>
+      @endforeach
+     
+    </tbody>
+  </table>
+</div> 
+
+@endsection
