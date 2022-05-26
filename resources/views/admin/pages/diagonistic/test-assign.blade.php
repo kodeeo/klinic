60d278be0ed9a39222a1cc85d75af8d2413fd2d3
Miftahur23 <miftahur109970@example.com>
2022-05-26 15:53:41 +0000

    modified

--- resources/views/admin/pages/diagonistic/test-assign.blade.php
+++ resources/views/admin/pages/diagonistic/test-assign.blade.php
@@ -12,14 +12,14 @@
 
                 @if(session()->has('key'))
                 {{-- @dd(session('key')) --}}
-                {{-- @foreach (session('cart') as $id => $test)
+                @foreach (session('cart') as $id => $test)
                             {{-- @dd(session('cart')) --}}
     
-                                {{-- @php
+                                @php
                                     $subtotal = $test['price'];
                                     $total += $subtotal;
                                 @endphp
-                                @endforeach --}}
+                                @endforeach
 
             
     
@@ -34,34 +34,37 @@
                 <div class="card" style="margin-left:5px; margin-right:5px; margin-bottom:5px; margin-top:2px; padding-left: 10%; padding-right: 10%; padding-top: 2%; padding-bottom: 5%; background:aqua;" >
                     <h3>Total Due</h3>
                     <br>
-                    <h4>BDT {{$total}}.00 /- </h4>
+                    <h4 id="total">{{$total}} </h4>
                 </div>
 
                 <div class="card" style="margin-left:5px; margin-right:5px; margin-bottom:5px; margin-top:2px; padding-left: 10%; padding-right: 10%; padding-top: 2%; padding-bottom: 5%; background:orange;" >
                     <h3>Change</h3>
                     <br>
-                    <h4>BDT {{$change}}.00 /- </h4>
+                    <h4 id="change">BDT 00 /- </h4>
 
                 </div>
             </div>
 
 
-            <div class="container" style="display: flex; justify-content:center; background:#27a6a8; padding-bottom:5px; ">
-                <div style="margin:5px;">
-                    <h3>Amount</h3>
-                    <br>
-                    <input type="text" id="name" name="name">
+            <form action="#" method="POST" style="background: #27a6a8; margin-top:5%; border-radius:10px;">
+                <div style="display: flex;">
+                    <div class="col-md-6 mb-4" style="margin-top:5%;">
+                        <label for="exampleInputAmount"><h3>Amount</h3></label>
+                        <input type="text" id="amount" name="amount" onkeyup="calculate()">
+                    </div>
+                
+                    <div class="col-md-6 mb-4" style="margin-top:5%;">
+                            <label for="method"><h3>Payment Method</h3></label>
+                            <select name="method" id="method" style="padding-right:50px; ">
+                                <option value="volvo">Cash</option>
+                                <option value="saab">Digital Banking</option>
+                            </select>                    
+                    </div>
                 </div>
-                    
-                <div style="margin:5px;">
-                    <h3>Payment Method</h3>
-                    <br>
-                    <select name="method" id="name" style="padding-right:50px; ">
-                        <option value="volvo">Cash</option>
-                        <option value="saab">Digital Banking</option>
-                    </select>
-                </div>
-            </div>
+                
+        
+                <button type="submit" class="btn btn-success" style="margin-bottom: 2%;">Pay</button>
+            </form>
         
         </div>
     
@@ -177,6 +180,16 @@
 
 </div>
 
+<script type="text/javascript">
+
+function calculate() {
+ var amount=document.getElementById("amount").value;
+ var total=document.getElementById("total").innerHTML;
+ 
+ document.getElementById("change").innerHTML=amount-total;
+}
+
+</script>
 
 
 
