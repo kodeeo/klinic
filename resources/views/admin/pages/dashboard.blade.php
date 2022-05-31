e7ef443d9ee7d30ae868e2da8203d4f2e4305469
Miftahur23 <miftahur109970@example.com>
2022-05-31 15:36:34 +0000

    dashboard fixed

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -1,19 +1,19 @@
 @extends('admin.master')
 @section('content')
 
-<div class="col-3" style="display: flex;">
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('patient.list')}}"><i class="fa-solid fa-truck-medical"></i> Patient Admission</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('patient.add')}}"><i class="fa-solid fa-person-circle-plus"></i> Add Patient</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('doctor.create')}}"><i class="fa fa-user-doctor"></i> Add Doctor</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('show.department')}}"><i class=""></i> Add Department</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('nurse.add')}}"><i class="fa fa-user-nurse"></i> Add Nurse</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('wardboy.add')}}"><i class=""></i> Add Wardboy</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('cabin.create')}}"><i class=""></i> Add Cabin</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="#"><i class=""></i> Add Ward</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="#"><i class="fa-solid fa-bed"></i> Add Bed</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('test.add')}}"><i class=""></i> Add Test Category</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('test.category.add')}}"><i class="fa-solid fa-syringe"></i> Add Test</a></div>
-    <div class="card-body" style="padding:3px;"><a class="btn btn-primary" href="{{route('cart.index')}}"><i class="fa-solid fa-syringe"></i> Assign Test</a></div>
+<div class="container" style="display: flex; margin-left: -30px;">
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('patient.list')}}"><i style="font-size: 30px;" class="fa fa-wheelchair"></i> Patient Admission</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('patient.add')}}"><i style="font-size: 30px;" class="fa fa-truck-medical"></i> Add Patient</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('doctor.create')}}"><i style="font-size: 30px;" class="fa fa-user-doctor"></i> Add Doctor</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('show.department')}}"><i style="font-size: 35px;" class="fa fa-apartment"></i> Add Department</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('nurse.add')}}"><i style="font-size: 30px;" class="fa fa-user-nurse"></i> Add Nurse</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('wardboy.add')}}"><i style="font-size: 30px;" class="fa fa-person-digging"></i> Add Wardboy</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('cabin.index')}}"><i style="font-size: 30px;" class=""></i> Add Cabin</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="#"><i style="font-size: 30px;" class="fa fa-bed-bunk"></i> Add Ward</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="#"><i style="font-size: 30px;" class="fa-solid fa-bed"></i> Add Bed</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('test.add')}}"><i style="font-size: 30px;" class=""></i> Add Test Category</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('test.category.add')}}"><i style="font-size: 30px;" class="fa-solid fa-syringe"></i> Add Test</a></div>
+    <div class="card-body" style="padding:3px;"><a class="btn btn-success" href="{{route('cart.index')}}"><i style="font-size: 30px;" class="fa-solid fa-syringe"></i> Assign Test</a></div>
 </div>
 
 @endsection
