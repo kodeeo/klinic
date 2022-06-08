0c1667528ce1376aeb1452352753150e2187c06b
Miftahur23 <miftahur109970@gmail.com>
2022-06-09 02:22:41 +0600

    recovered methods

--- app/Http/Controllers/Admin/Activities/InvestigationController.php
+++ app/Http/Controllers/Admin/Activities/InvestigationController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Admin\Activities;
 
+use App\Models\Doctor;
 use Illuminate\Http\Request;
 use App\Models\Investigation;
 use App\Http\Controllers\Controller;
+use Brian2694\Toastr\Facades\Toastr;
 
 class InvestigationController extends Controller
 {
@@ -26,7 +28,8 @@ class InvestigationController extends Controller
      */
     public function create()
     {
-        //
+        $doctor=Doctor::all();
+        return view('admin.pages.hospital activities.investigation.create',compact('doctor'));
     }
 
     /**
@@ -37,7 +40,24 @@ class InvestigationController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //dd($request->all());
+        $image_name=null;
+        if($request->hasFile('investigation_img'))
+        {
+            $image_name=date('Ymdhis').'.'.$request->file('investigation_img')->getClientOriginalExtension();
+            $request->file('investigation_img')->storeAs('/uploads/investigations',$image_name);
+        }
+
+        Investigation::create([
+            'patient_id'=>$request->patient_id,
+            'date' => $request->date,
+            'title' => $request->title,
+            'description' => $request->description,
+            'doctor_id'=>$request->doctor_name,
+            'investigation_img'=>'image',
+            'investigation_img'=>$image_name,
+        ]);
+        return redirect()->route('investigations.index')->with(Toastr::success('investigation created successfully'));
     }
 
     /**
@@ -48,7 +68,8 @@ class InvestigationController extends Controller
      */
     public function show($id)
     {
-        //
+        $investigations=Investigation::find($id);
+        return view('admin.pages.hospital activities.investigation.view', compact('investigations'));
     }
 
     /**
@@ -59,7 +80,9 @@ class InvestigationController extends Controller
      */
     public function edit($id)
     {
-        //
+        $investigations=Investigation::find($id);
+        $doctor = Doctor::all();
+        return view('admin.pages.hospital activities.investigation.edit', compact('investigations','doctor'));
     }
 
     /**
@@ -71,7 +94,25 @@ class InvestigationController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $investigations=Investigation::find($id);
+
+        $image_name=$investigations->investigation_img;
+        if($request->hasFile('investigation_img'))
+        {
+            $image_name=date('Ymdhis').'.'.$request->file('investigation_img')->getClientOriginalExtension();
+            $request->file('investigation_img')->storeAs('/uploads/investigations',$image_name);
+        }
+
+        $investigations->update([
+            'patient_id'=>$request->patient_id,
+            'date' => $request->date,
+            'title' => $request->title,
+            'description' => $request->description,
+            'doctor_id'=>$request->doctor_name,
+            'investigation_img'=>'image',
+            'investigation_img'=>$image_name,
+        ]);
+        return redirect()->route('investigations.index')->with(Toastr::info('investigation updated successfully'));
     }
 
     /**
@@ -82,6 +123,7 @@ class InvestigationController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Investigation::find($id)->delete();
+        return redirect()->route('investigations.index')->with(Toastr::error('investigation has been deleted'));
     }
 }
