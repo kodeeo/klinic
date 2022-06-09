1a16bbf277bc22a503e0c227bcdf857510617099
Tifoysal <foysal11203004@gmail.com>
2022-06-09 19:37:56 +0600

    update logo

--- app/Models/ClinicSetup.php
+++ app/Models/ClinicSetup.php
@@ -14,7 +14,7 @@ class ClinicSetup extends Model
     public function getImageAttribute($image)
     {
         if($image){
-            return Storage::url('/klinic/'.$image);
+            return Storage::url('/kliniclogo/'.$image);
         }
     }
 }
