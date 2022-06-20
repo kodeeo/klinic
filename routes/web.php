b67e54750981129833612f257087b6fc4b19d763
Miftahur23 <miftahur109970@gmail.com>
2022-06-20 23:01:56 +0600

    route fixd

--- routes/web.php
+++ routes/web.php
@@ -63,10 +63,7 @@ use App\Http\Controllers\Admin\Export\HospitalActivitiesExportController;
 
 use App\Http\Controllers\Admin\InsuranceController;
 
-
-use App\Http\Controllers\Admin\Export\HospitalActivitiesExportController;
 use App\Http\Controllers\Admin\PackageController;
-use App\Models\Test;
 
 
 /*
