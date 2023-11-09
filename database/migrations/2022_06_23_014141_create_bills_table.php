a867da63f709b1f35cae7179870ad2189cbcf4ac
statementloop <rimizoo468@gmail.com>
2023-11-09 12:28:17 +0600

    nurse

--- database/migrations/2022_06_23_014141_create_bills_table.php
+++ database/migrations/2022_06_23_014141_create_bills_table.php
@@ -11,22 +11,7 @@ return new class extends Migration
     {
         Schema::create('bills', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('pastient_id');
-            $table->foreignId('admission_id')->nullable();
-            $table->string('bill_id')->unique();
-            $table->double('pay_advance')->nullable();
-            $table->date('bill_date');
-            $table->double('amount');
-            $table->string('payment_method');
-            $table->string('card_cheque_number')->nullable();
-            $table->string('receipt_number')->nullable();
-            $table->integer('discount')->nullable();
-            $table->double('total_payable');
-            $table->foreignId('service_id');
-            $table->foreignId('package_id')->nullable();
-            $table->text('remarks');
-            $table->string('note')->nullable();
-            $table->string('status')->default('0');
+
             $table->timestamps();
 
 
