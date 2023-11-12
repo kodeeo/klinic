11b790513087ecee0e10e63ad00f7ec096594f75
Maksud Bhuiyan <maksudbhuiyan595>
2023-11-12 21:14:42 +0600

    local change

--- database/migrations/2022_04_05_132639_create_patients_table.php
+++ database/migrations/2022_04_05_132639_create_patients_table.php
@@ -24,16 +24,12 @@ return new class extends Migration
             $table->date('date_of_birth')->nullable();
             $table->string('gender',20);
             $table->string('blood_group')->nullable();
-<<<<<<< HEAD
-            $table->string('patient_image',50);
-            $table->text('address')->nullable();
-            $table->string('status',15)->default('registered');
-=======
+
             $table->string('patient_image')->nullable();
             $table->text('address')->nullable();
             $table->string('status',15)->default('active');
             $table->string('ref_by')->nullable()->comment('refered doctor name with speciality');
->>>>>>> origin
+
             $table->timestamps();
             $table->softDeletes();
         });
